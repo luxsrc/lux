69921c127b3ac50851d3476c26003032a7b3d71d
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2009-07-26 16:25:50 -0400

    A minimal program that prints the name and version of this package

---
+++ src/main.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2009 Chi-kwan Chan
+ * Copyright (C) 2009 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <lux.h>
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+	return EOF == puts("lux ("PACKAGE_NAME") commit '"PACKAGE_VERSION"'");
+
+	(void)argc; /* silence unused parameter warning */
+	(void)argv; /* silence unused parameter warning */
+}
