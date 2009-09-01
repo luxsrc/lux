3335a45787e37befbb2061b018f6d3f243d00ac6
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2009-08-31 21:55:40 -0400

    Try to load module from the current work directory first

--- api/load.c
+++ api/load.c
@@ -20,31 +20,50 @@
 #include <lux.h>
 #include <lux/lazybuf.h>
 #include <lux/load.h>
+#include <unistd.h> /* for getcwd() */
 #include <string.h> /* for strcat(), strcpy(), and strlen() */
 #include <stdarg.h>
 
-static const char *path = LUX_MOD_PATH;
+#define COUNT_OF(a) (sizeof(a) / sizeof(a[0]))
+
+static const char *paths[] = {NULL, LUX_MOD_PATH};
+
+static inline size_t
+max(size_t a, size_t b)
+{
+	return a > b ? a : b;
+}
 
 void *
 lux_load(const char *restrict name, ...)
 {
+	size_t i, maxlen;
+
 	char lazybuf[256], *buf;
 
 	va_list ap;
 	void   *obj;
 
+	if(!paths[0])
+		paths[0] = getcwd(NULL, 0);
+
+	for(i = 1, maxlen = strlen(paths[0]); i < COUNT_OF(paths); ++i)
+		maxlen = max(maxlen, strlen(paths[i]));
+
 	/* Try to allocate more memory if name is too long */
-	buf = (char *)MALLOC(strlen(path) + sizeof("/") + strlen(name));
+	buf = (char *)MALLOC(maxlen + sizeof("/") + strlen(name));
 	if(!buf) {
 		lux_error("lux_load(\"%s\"): failed to allocate string\n",
 		          name);
 		return NULL;
 	}
-	(void)strcat(strcat(strcpy(buf, path), "/"), name);
 
 	/* Try to load the module */
 	va_start(ap, name);
-	obj = vload(buf, ap);
+	for(i = 0, obj = NULL; i < COUNT_OF(paths) && !obj; ++i) {
+		(void)strcat(strcat(strcpy(buf, paths[i]), "/"), name);
+		obj = vload(buf, ap);
+	}
 	va_end(ap);
 
 	FREE(buf);
