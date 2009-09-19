3ba128531f59d814851278c1fdd10e783ca7aa29
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2009-09-19 03:18:55 -0400

    Add <lux/match.h> to help implementing other sims

---
+++ mod/match.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2009 Chi-kwan Chan
+ * Copyright (C) 2009 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _LUX_MATCH_H_
+#define _LUX_MATCH_H_
+
+#include <string.h>
+
+const char *match(const char *sym, const char *arg)
+{
+	size_t i;
+
+	const size_t l = strlen(sym);
+	const size_t n = strlen(arg);
+
+	if(l + 1 > n || '=' != arg[l])
+		return NULL;
+
+	for(i = 0; i < l; ++i)
+		if(sym[i] != arg[i])
+			return NULL;
+
+	return arg + l + 1;
+}
+
+#endif /* _LUX_MATCH_H_ */
