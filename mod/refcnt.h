eee788721d2c9777b422fb62067f97e95775ccd9
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2009-12-28 15:38:21 -0500

    Introduce a simple reference counting module

---
+++ mod/refcnt.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2009 Chi-kwan Chan
+ * Copyright (C) 2009 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _LUX_REFCNT_H_
+#define _LUX_REFCNT_H_
+
+#define REF_NULL {0}
+
+struct ref {
+	int cnt; /* assume int is atomic */
+};
+
+static inline void
+refcnt_set(struct ref *ref, int cnt)
+{
+	ref->cnt = cnt;
+}
+
+static inline int
+refcnt_get(struct ref *ref)
+{
+	return ref->cnt;
+}
+
+static inline int
+refcnt_inc(struct ref *ref)
+{
+	return ++ref->cnt;
+}
+
+static inline int
+refcnt_dec(struct ref *ref)
+{
+	return --ref->cnt;
+}
+
+#endif /* _LUX_REFCNT_H_ */
