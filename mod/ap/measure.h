7ebbeab662dda1e22dd86a1c67a4eccde3812bed
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2009-12-28 17:48:10 -0500

    Follow new convention to rename <lux/ap/cycle.h> to <lux/ap/ticks.h>

--- mod/ap/measure.h
+++ mod/ap/measure.h
@@ -20,8 +20,8 @@
 #ifndef _LUX_MEASURE_H_
 #define _LUX_MEASURE_H_
 
-#include <lux/ap/cycle.h>
 #include <lux/ap/task.h>
+#include <lux/ap/ticks.h>
 #include <lux/ap/timer.h>
 #include <math.h>
 
@@ -29,7 +29,7 @@
 # error high resolution tick counter is not available on this platform
 #else
 # ifdef TIME_MIN
-#  define TICK_MIN TIME_MIN /* defined in <lux/ap/cycle.h> in ticks */
+#  define TICK_MIN TIME_MIN /* defined in <lux/ap/ticks.h> in ticks */
 # else
 #  define TICK_MIN 100.0 /* from "fftw3/kernel/timer.c" */
 # endif
