a98bf67430986ff608aa1026ce70908c6cb7f955
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2009-12-29 10:36:28 -0500

    Put declaration of fopencookie() and related types into "mod/cookie.h"

--- sys/fopencookie.c
+++ sys/fopencookie.c
@@ -18,6 +18,7 @@
  * along with lux.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <lux.h>
+#include <lux/cookie.h>
 
 #if HAVE_FOPENCOOKIE
 /* Use the system fopencookie(); implement nothing */
