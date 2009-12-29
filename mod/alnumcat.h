878adb8fdf870c39396da497a7a49bb69d500081
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2009-12-28 22:19:30 -0500

    Implement a static module that appends alphanumeric characters

---
+++ mod/alnumcat.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2009 Chi-kwan Chan
+ * Copyright (C) 2009 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _LUX_ALNUMCAT_H_
+#define _LUX_ALNUMCAT_H_
+
+#include <ctype.h>
+#include <string.h>
+
+static inline char *
+alnumcat(char *d, const char *s)
+{
+	char *t = d + strlen(d);
+	while(isalnum(*s))
+		*t++ = *s++;
+	return d;
+}
+
+#endif /* _LUX_ALNUMCAT_H_ */
