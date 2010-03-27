5560ed1104ef7c913d8ea61e9f9727f0b6525e38
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-03-26 23:54:25 -0400

    Move the external MPI interface to the main lux repository

---
+++ mod/mpi.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2010 Chi-kwan Chan
+ * Copyright (C) 2010 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _LUX_MPI_H_
+#define _LUX_MPI_H_
+
+#include <mpi.h>
+
+typedef struct LuxSmpi Lux_mpi;
+
+struct LuxSmpi {
+	MPI_Comm super;
+};
+
+#endif /* _LUX_MPI_H_ */
