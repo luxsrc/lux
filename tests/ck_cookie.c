dd98aaaabfab9bb0ab188fecdab7575cbaed5a7d
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-03-28 19:35:18 -0400

    Test read and write functions in cookie

--- tests/ck_cookie.c
+++ tests/ck_cookie.c
@@ -25,28 +25,64 @@
 #include <lux/assert.h>
 #include <lux/cookie.h>
 #include <stdio.h>
+#include <string.h>
 
-static char cookie[1024];
+typedef struct {
+	size_t current, count;
+	char data[1024];
+} cookie_t;
 
 static ssize_t
-dummy(void *c, const char *buf, size_t sz)
+read(void *c, char *buf, size_t sz)
 {
-	return 0;
+	char *data = ((cookie_t *)c)->data;
+
+	size_t i = 0;
+	size_t j = ((cookie_t *)c)->current;
+	size_t n = ((cookie_t *)c)->count;
+	for(; i < sz && j < n; ++i, ++j)
+		buf[i] = data[j%1024];
+
+	((cookie_t *)c)->current = j;
+	return i;
+}
+
+static ssize_t
+write(void *c, const char *buf, size_t sz)
+{
+	char  *data = ((cookie_t *)c)->data;
+
+	size_t i =  0;
+	size_t j = ((cookie_t *)c)->current;
+	for(; i < sz; ++i, ++j)
+		data[j%1024] = buf[i];
+
+	((cookie_t *)c)->current = j;
+	((cookie_t *)c)->count   = j;
+	return i;
 }
 
 int
 main()
 {
+	char out[1024] = "123";
+	cookie_t cookie = {0};
 	cookie_io_functions_t funcs = {0};
 	FILE *f;
 
-	funcs.read = dummy;
-	f = fopencookie(cookie, "w+", funcs);
+	funcs.read = read;
+	f = fopencookie(&cookie, "w+", funcs);
 	lux_assert(f == NULL);
 
-	funcs.write = dummy;
-	f = fopencookie(cookie, "w+", funcs);
+	funcs.write = write;
+	f = fopencookie(&cookie, "w+", funcs);
 	lux_assert(f != NULL);
+
+	fwrite("abc", 1, 4, f); fflush(f);
+	lux_assert(strcmp(cookie.data, "abc") == 0);
+
+	fread(out, 1, 4, f);
+	lux_assert(strcmp(out, "123") == 0);
 
 	lux_assert(fclose(f) == 0);
 
