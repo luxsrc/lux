cd6d961cfa0c008cd57d01e54e33ce6a150490a3
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-03-28 19:11:16 -0400

    Add a test to check fopencookie()

---
+++ tests/ck_cookie.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2010 Chi-kwan Chan
+ * Copyright (C) 2010 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LUX_ASSERTION
+#define LUX_ASSERTION 1
+#endif
+
+#include <lux.h>
+#include <lux/assert.h>
+#include <lux/cookie.h>
+#include <stdio.h>
+
+static char cookie[1024];
+
+int
+main()
+{
+	cookie_io_functions_t cookie_func = {NULL, NULL, NULL, NULL};
+	FILE *f;
+
+	lux_assert(f = fopencookie(cookie, "w+", cookie_func));
+	lux_assert(fclose(f) == 0);
+
+	return 0;
+}
