3615219612e4b823823f534e8949d39d0db034e7
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-03-28 21:19:37 -0400

    Add a test to check <lux/clist.h>

---
+++ tests/ck_clist.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2010 Chi-kwan Chan
+ * Copyright (C) 2010 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LUX_ASSERTION
+#define LUX_ASSERTION 1
+#endif
+
+#include <lux.h>
+#include <lux/assert.h>
+#include <lux/clist.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node {
+	struct cnode super;
+	int value;
+};
+
+static inline struct node *
+mknode(int value)
+{
+	struct node *n = malloc(sizeof(struct node));
+	clist_init(&n->super);
+	n->value = value;
+	return n;
+}
+
+int
+main()
+{
+	int i, n = 10, offset;
+
+	struct node *h = mknode(-1);
+
+	offset = 100;
+	for(i = 0; i < n; ++i)
+		clist_ins(&h->super, &mknode(offset+i)->super);
+	for(i = 0; i < n+1; ++i) {
+		struct node *c = (struct node *)clist_del(&h->super);
+		if(c == h) {
+			printf("empty\n");
+			lux_assert(c->value == -1);
+		} else {
+			printf("%d ", c->value);
+			lux_assert(c->value == offset+(n-1-i));
+			free(c);
+		}
+	}
+
+	offset = 110;
+	for(i = 0; i < n; ++i)
+		clist_ins(&h->super, &mknode(offset+i)->super);
+	for(i = 0; i < n+1; ++i) {
+		struct node *c = (struct node *)clist_del(&h->super);
+		if(c == h) {
+			printf("empty\n");
+			lux_assert(c->value == -1);
+		} else {
+			printf("%d ", c->value);
+			lux_assert(c->value == offset+(n-1-i));
+			free(c);
+		}
+	}
+
+	free(h);
+
+	return 0;
+}
