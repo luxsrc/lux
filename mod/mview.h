022f2f8eb0e10bcd370c749505bb4a7c1739f692
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-03-30 01:32:18 -0400

    Start implement a portable way to create a view on the memory pool

---
+++ mod/mview.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2010 Chi-kwan Chan
+ * Copyright (C) 2010 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _LUX_MVIEW_H_
+#define _LUX_MVIEW_H_
+
+#include <lux/mpool.h>
+#include <unistd.h>
+
+static inline void *
+mkmview(struct mpool *mp, size_t lower, size_t length, unsigned flags)
+{
+	if(mp->sz < upper) {
+		size_t sz  = ((mp->sz + mp->psz - 1) / mp->psz) * mp->psz;
+		int    err = ftruncate(mp->fd, mp->sz);
+		if(!err)
+			mp->sz = sz;
+	}
+	return mmap(NULL, length, flags, mp->fd, lower);
+}
+
+static inline void
+rmview(struct mpool *mp, void *v, size_t length)
+{
+	(void)munmap(v, length);
+}
+
+#endif /* _LUX_MVIEW_H_ */
