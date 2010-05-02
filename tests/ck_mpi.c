43730adfb019549da1bcd8ae79779562e9a4926e
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-05-02 11:35:21 -0400

    Add a test to check the MPI dynamic module
    For the current implementation, `make install` is needed before `make
    check` for the test to run successfully.

---
+++ tests/ck_mpi.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2010 Chi-kwan Chan
+ * Copyright (C) 2010 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LUX_ASSERTION
+#define LUX_ASSERTION 1
+#endif
+
+#include <lux.h>
+#include <lux/mpi.h>
+#include <stdio.h>
+
+int main()
+{
+	Lux_mpi *mpi;
+
+	lux_setup();
+
+	mpi = lux_load("mpi", NULL);
+
+	switch(mpi->thread_mode) {
+	case MPI_THREAD_SINGLE:     lux_print("single\n");     break;
+	case MPI_THREAD_FUNNELED:   lux_print("funneled\n");   break;
+	case MPI_THREAD_SERIALIZED: lux_print("serialized\n"); break;
+	case MPI_THREAD_MULTIPLE:   lux_print("multiple\n");   break;
+	}
+
+	lux_unload(mpi);
+
+	return 0;
+}
