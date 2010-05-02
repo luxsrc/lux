4d3fbc019da4647cd241429d6aab5e867f485202
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-05-02 02:18:33 -0400

    Remove "mod/vector.h"; move documentation/comments to "mod/tensor.h"

--- mod/tensor.h
+++ mod/tensor.h
@@ -20,16 +20,81 @@
 #ifndef _LUX_TENSOR_H_
 #define _LUX_TENSOR_H_
 /*
- * Similar to <lux/vector.h>, there are different ways to keep track
- * of the shapes of multi-dimensional arrays that makes sense in
- * standard C.  For convenience, we call these arrays "tensors" so
- * that their dimensions are naturally called "rank", and their shapes
- * are naturally called "dimensions".
+ * There are 6 ways to keep track of the shapes/lengths/counts of
+ * arrays that make sense in standard C.  For convenience, we use 1D
+ * arrays (vectors) as example here:
  *
- * We do not list all the possible ways here.  The basic principle is
- * the same as <lux/vector.h>, that we need to keep track both a
- * header that contains the rank and dimension information, and the
- * actual data of the tensor.
+ * 1. Use two variables manually: GOOD if everything is in the same
+ *    scope; cannot be returned; BAD to pass as argument
+ *
+ *	size_t n;
+ *	char   v[n];
+ *
+ * 2. Use pointer for the vector: FINE if everything is in the same
+ *    scope; cannot be returned; OK to pass as argument
+ *
+ *	size_t n;
+ *	char  *v; // -> char _v[n];
+ *
+ * 3. struct with flexible array element: same as 1---large struct to
+ *    passed (for pointer to this struct, see method 5)
+ *
+ *	struct {
+ *		size_t n;
+ *		char   v[n];
+ *	} v;
+ *
+ * 4. struct with pointer: FINE to return result; GOOD to pass
+ *    argument
+ *
+ *	struct {
+ *		size_t n;
+ *		char  *v; // -> char _v[n];
+ *	} v;
+ *
+ * 5. Pointer to struct with flexible array element: GOOD to return
+ *    result, FINE to pass argument
+ *
+ *	struct {
+ *		size_t n;
+ *		char   v[n];
+ *	} *v;
+ *
+ * 6. Pointer to struct with pointer: BAD because of double
+ *    de-referencing---not much point to use it
+ *
+ *	struct string {
+ *		size_t n;
+ *		char  *v; // -> char _v[n];
+ *	} *v;
+ *
+ * Traditionally, C APIs use methods 2, which can only be "returned"
+ * by using arguments:
+ *
+ *	void mkvect(size_t *n_out, char **v_out);
+ *
+ * and is cumbersome.  In lux, we have used both methods 4 and 5,
+ * depending on if passing or returning is more important.
+ *
+ * For this static module, we introduce yet another method using a
+ * "hack".  The vector is allocated in memory using method 3.  But
+ * instead of referencing is using method 5, we return the reference
+ * to the flexible length array.  I.e.,
+ *
+ * 7. "headerof" method: GOOD to return result and pass result; act
+ *     like normal pointers; slightly slower in order to look up
+ *     length
+ *
+ *	struct {
+ *		size_t n;
+ *		char   v[n];
+ *	} _v; // in memory
+ *
+ *	char *v = _v.v; // pointer to v[] instead of _v.
+ *
+ * In fact, because of the "headerof" method return ordinary a
+ * pointer, it can be combined with method 2 and 4, which makes it
+ * very flexible.
  *
  * In principle, we can let tfree() and tgetn() determine the rank in
  * runtime.  Nevertheless, we intend to use <lux/tensor.h> even in
