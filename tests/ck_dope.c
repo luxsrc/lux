0a25c80b680e42bb446a09cd61ab4c7c667c9af8
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-05-02 23:17:45 -0400

    Add a test to check <lux/dope.h>

---
+++ tests/ck_dope.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2010 Chi-kwan Chan
+ * Copyright (C) 2010 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LUX_ASSERTION
+#define LUX_ASSERTION 1
+#endif
+
+#include <lux.h>
+#include <lux/assert.h>
+#include <lux/dope.h>
+#include <lux/parray.h>
+#include <stdio.h>
+
+#define ALIGN 256
+#define N1 5
+#define N2 7
+#define N3 11
+
+int
+main()
+{
+	struct dope *dp = mkdope(double [5], ALIGN, N1, N2, N3);
+
+	printf("dim: %zu\n", pgetd(dp));
+	lux_assert(pgetd(dp) == 1);
+
+	printf("shape: %zu %zu %zu\n",
+	       dope_getn(dp+0), dope_getn(dp+1), dope_getn(dp+2));
+	lux_assert(dope_getn(dp+0) == N1);
+	lux_assert(dope_getn(dp+1) == N2);
+	lux_assert(dope_getn(dp+2) == N3);
+
+	printf("strides: %zu %zu %zu\n",
+	       dope_gets(dp+0), dope_gets(dp+1), dope_gets(dp+2));
+	lux_assert(dope_gets(dp+0) == sizeof(double [5]));
+	lux_assert(dope_gets(dp+1) == ALIGN);
+	lux_assert(dope_gets(dp+2) == ALIGN * N2);
+
+	rmdope(dp);
+	return 0;
+}
