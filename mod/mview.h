96537d5c6b80f9a213f81d4de158faf81f17c375
Chi-kwan Chan <ckchan@cfa.harvard.edu>
2010-05-02 20:25:34 -0400

    Reimplement mkmview() to take dope vector as an input

--- mod/mview.h
+++ mod/mview.h
@@ -21,17 +21,30 @@
 #define _LUX_MVIEW_H_
 
 #include <lux/assert.h>
+#include <lux/dope.h>
 #include <lux/mpool.h>
+#include <lux/parray.h>
 #include <unistd.h>
 
 static inline void *
-mkmview(struct mpool *mp, size_t lower, size_t length)
+mkmview(struct mpool *mp, size_t off, struct dope *dp)
 {
-	lux_assert(mp->sz >= lower+length);
+	size_t n = pgetn(dp, 0);
+	size_t i, objsz, length;
+	for(i=0, objsz=-1, length=0; i < n; ++i) {
+		size_t s = dope_gets(dp+i);
+		size_t n = dope_getn(dp+i);
+		length  += s * (n-1);
+		objsz    = s < objsz ? s : objsz;
+	}
+	length += objsz;
+
+	lux_assert(mp->sz >= off+length);
+
 	return mmap(NULL, length,
 	            PROT_READ | PROT_WRITE,
 	            MAP_SHARED,
-	            mp->fd, lower);
+	            mp->fd, off);
 }
 
 static inline void
