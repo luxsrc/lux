47952ebd8e92114bf313ff2e9d936b59d5354575
Chi-kwan Chan <chanc@email.arizona.edu>
2014-01-01 19:45:50 -0500

    Add a test to check both <lux/ticks.h> and <lux/timer.h>

---
+++ tests/ck_ticks_timer.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2014 Chi-kwan Chan
+ * Copyright (C) 2014 Steward Observatory
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LUX_ASSERTION
+#define LUX_ASSERTION 1
+#endif
+
+#include <lux.h>
+#include <lux/assert.h>
+#include <lux/ticks.h>
+#include <lux/timer.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main()
+{
+	timestamp_t T0, T1;
+	ticks_t     t0, t1;
+	double      dt, dT;
+
+	t0 = getticks();
+	T0 = gettimestamp();
+	usleep(1000);
+	T1 = gettimestamp();
+	t1 = getticks();
+
+	dT = elapsed_time(T1, T0);
+	dt = elapsed(t1, t0);
+
+	printf("dtimestamps: %g\n", dT);
+	printf("dticks     : %g\n", dt);
+	printf("ticks/sec  : %g\n", dt/dT);
+
+	return 0;
+}
