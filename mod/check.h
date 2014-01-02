04b0540edbd885f8ab75d92295570277fea71d89
Chi-kwan Chan <chanc@email.arizona.edu>
2014-01-01 22:16:08 -0500

    Improve code readability for <lux/check.h>

--- mod/check.h
+++ mod/check.h
@@ -22,41 +22,45 @@
 
 #include <lux/failed.h>
 
+/*
+ * Fail if E returns a non-zero value; useful in checking errno or failed
+ *
+ * Note that we explicitly cast E to int.  This is intentional because
+ * this macro checks for failure code, which has type int.
+ */
 #ifndef LUX_CHECK_INFAILURE
 #define LUX_CHECK_INFAILURE 0
 #endif
 
+#define lux_check_failure_code(E, L) do {                                   \
+	int __f__ = (int)(E);                                               \
+	if(__f__ != LUX_CHECK_INFAILURE) {                                  \
+		lux_error("lux_check_failure_code(" #E ", " #L ") failed "  \
+		          "with error code %d on line %d in \"%s\"\n",      \
+		          __f__, __LINE__, __FILE__);                       \
+		goto L;                                                     \
+	}                                                                   \
+} while(0)
+
+/*
+ * Fail if E returns zero; useful in checking memory allocations
+ *
+ * Unlike lux_check_failure_code(), we avoid explicitly casting E to
+ * any type.
+ */
 #ifndef LUX_CHECK_UNSUCCESS
 #define LUX_CHECK_UNSUCCESS 0
 #endif
 
-/* Fail if E returns a non-zero value; useful in checking errno or failed
-
-   Note that we explicitly cast E to int.  This is intentional because
-   this macro checks for failure code, which has type int.  */
-#define lux_check_failure_code(E, L) do {                              \
-	int __f__ = (int)(E);                                          \
-	if(__f__ != LUX_CHECK_INFAILURE) {                             \
-		lux_error("lux_check_failure(" #E ", " #L ") failed "  \
-		          "with error code %d on line %d in \"%s\"\n", \
-		          __f__, __LINE__, __FILE__);                  \
-		goto L;                                                \
-	}                                                              \
-} while(0)
-
-/* Fail if E returns zero; useful in checking memory allocations
-
-   Unlike lux_check_failure_code(), we avoid explicitly casting E to
-   any type.  */
-#define lux_check_func_success(E, L) do {                              \
-	int __fsv__ = failed;                                          \
-	if((E) == LUX_CHECK_UNSUCCESS) {                               \
-		lux_error("lux_check_success(" #E ", " #L ") failed "  \
-		          "with error code %d on line %d in \"%s\"\n", \
-		          failed, __LINE__, __FILE__);                 \
-		failed = __fsv__;                                      \
-		goto L;                                                \
-	}                                                              \
+#define lux_check_func_success(E, L) do {                                  \
+	int __fsv__ = failed;                                              \
+	if((E) == LUX_CHECK_UNSUCCESS) {                                   \
+		lux_error("lux_check_func_success(" #E ", " #L ") failed " \
+		          "with error code %d on line %d in \"%s\"\n",     \
+		          failed, __LINE__, __FILE__);                     \
+		failed = __fsv__;                                          \
+		goto L;                                                    \
+	}                                                                  \
 } while(0)
 
 #endif /* _LUX_CHECK_H_ */
