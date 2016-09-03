49936418903e2232864804e4b309bbe4c7b8fbfa
Chi-kwan Chan <chanc@email.arizona.edu>
2016-09-03 03:59:08 -0700

    Implement Lux_hdf5->write_pa()

--- mod/hdf5/mod.c
+++ mod/hdf5/mod.c
@@ -22,16 +22,62 @@
 #include <lux/file.h>
 #include <lux/hdf5.h>
 #include <lux/io.h>
+#include <lux/parray.h>
 #include <lux/switch.h>
 #include <stdlib.h> /* for malloc(), free(), and NULL */
+#include <stdint.h> /* for int8_t, int16_t, ... */
 
 #define EGO ((Lux_hdf5 *)ego)
+
+#define set_d_ns(T) do {                   \
+	int i;                             \
+	d  = pgetd((T *)pa);               \
+	ns = malloc(sizeof(hsize_t) * d);  \
+	for(i = 0; i < d; ++i)             \
+		ns[i] = pgetn((T *)pa, i); \
+} while(0)
+
+static inline hid_t
+getdims(int tc, const void *pa)
+{
+	hid_t dims;
+
+	int      d;
+	hsize_t *ns;
+	switch(tc >> LUX_CHAR_BIT) {
+	case  8: set_d_ns(int8_t);  break;
+	case 16: set_d_ns(int16_t); break;
+	case 32: set_d_ns(int32_t); break;
+	case 64: set_d_ns(int64_t); break;
+	default: return -1;         break;
+	}
+	dims = H5Screate_simple(d, ns, NULL);
+	free(ns);
+
+	return dims;
+}
 
 static void
 close(Lux_file *ego)
 {
 	(void)H5Fclose(EGO->fid);
 	free(ego);
+}
+
+static void
+write_pa(Lux_file *ego, const char *key, int tc, const void *pa)
+{
+	hid_t type, dims, dset;
+
+	type = h5t_from_tc(tc);
+	dims = getdims(tc, pa);
+	dset = H5Dcreate(EGO->fid, key, type, dims,
+	                 H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+	(void)H5Dwrite(dset, type, H5S_ALL, H5S_ALL, H5P_DEFAULT, pa);
+
+	(void)H5Dclose(dset);
+	(void)H5Sclose(dims);
 }
 
 Lux_file *
@@ -52,7 +98,8 @@ LUX_MOD(const char *fname, unsigned flags)
 	if(EGO->fid < 0)
 		goto cleanup2;
 
-	ego->close = close;
+	ego->close    = close;
+	ego->write_pa = write_pa;
 	return ego;
 
 cleanup2:
