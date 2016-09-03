cd9a7739ef313427d09ccbeabef915fb56cb386a
Chi-kwan Chan <chanc@email.arizona.edu>
2016-09-03 02:08:24 -0700

    Implement inline function h5t_from_tc()to turn typecode to HDF5 type

--- mod/hdf5.h
+++ mod/hdf5.h
@@ -21,6 +21,7 @@
 #define _LUX_HDF5_H_
 
 #include <lux/file.h>
+#include <lux/switch.h>
 #include <hdf5.h>
 
 typedef struct LuxShdf5 Lux_hdf5;
@@ -29,5 +30,39 @@ struct LuxShdf5 {
 	Lux_file super;
 	hid_t    fid;
 };
+
+static inline hid_t
+h5t_from_tc(int tc)
+{
+	int sz   = (tc >> LUX_CHAR_BIT) / LUX_CHAR_BIT;
+	int type = tc & ((1 << LUX_CHAR_BIT) - 1);
+
+	switch(type) {
+	case 'i':
+		SWITCH {
+		CASE(sz == sizeof(char))        return H5T_NATIVE_CHAR;
+		CASE(sz == sizeof(short))       return H5T_NATIVE_SHORT;
+		CASE(sz == sizeof(int))         return H5T_NATIVE_INT;
+		CASE(sz == sizeof(long))        return H5T_NATIVE_LONG;
+		CASE(sz == sizeof(long long))   return H5T_NATIVE_LLONG;
+		}
+	case 'u':
+		SWITCH {
+		CASE(sz == sizeof(char))        return H5T_NATIVE_UCHAR;
+		CASE(sz == sizeof(short))       return H5T_NATIVE_USHORT;
+		CASE(sz == sizeof(int))         return H5T_NATIVE_UINT;
+		CASE(sz == sizeof(long))        return H5T_NATIVE_ULONG;
+		CASE(sz == sizeof(long long))   return H5T_NATIVE_ULLONG;
+		}
+	case 'f':
+		SWITCH {
+		CASE(sz == sizeof(float))       return H5T_NATIVE_FLOAT;
+		CASE(sz == sizeof(double))      return H5T_NATIVE_DOUBLE;
+		CASE(sz == sizeof(long double)) return H5T_NATIVE_LDOUBLE;
+		}
+	};
+
+	return -1;
+}
 
 #endif /* _LUX_HDF5_H_ */
