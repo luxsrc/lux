0a44efc8278e11aca9679669493ac8bc237a89f7
Chi-kwan Chan <chanc@email.arizona.edu>
2016-09-05 00:22:35 -0700

    Add a test to check copying OpenCL context

---
+++ tests/opencl/ck_copy.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2010 Chi-kwan Chan
+ * Copyright (C) 2010 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <lux.h>
+#include <lux/assert.h>
+#include <lux/opencl.h>
+#include <unistd.h>
+
+#define A(E) lux_always_assert(E)
+
+int
+main(int argc, char *argv[])
+{
+	struct LuxOopencl opts = OPENCL_NULL;
+	Lux_opencl *ocl1, *ocl2;
+
+	lux_setup(&argc, &argv);
+
+	ocl1 = lux_load("../../mod/opencl/.libs/opencl", &opts);
+	A(ocl1);
+
+	opts.ctx = ocl1->ctx;
+
+	ocl2 = lux_load("../../mod/opencl/.libs/opencl", &opts);
+	A(ocl2);
+
+	lux_unload(ocl2);
+	lux_unload(ocl1);
+
+	return 0;
+}
