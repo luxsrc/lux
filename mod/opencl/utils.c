d20be5acbe69ca9798d68017ac227a8ab71d8280
Chi-kwan Chan <chanc@email.arizona.edu>
2016-09-05 10:18:32 -0700

    Implement getdev() to get device from a queue

---
+++ mod/opencl/utils.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 Chi-kwan Chan
+ * Copyright (C) 2016 Steward Observatory
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <lux.h>
+#include "mod.h"
+
+cl_device_id
+getdev(cl_command_queue que)
+{
+	cl_device_id dev;
+	check(GetCommandQueueInfo,
+	      que, CL_QUEUE_DEVICE, sizeof(dev), &dev, NULL);
+	return dev;
+}
