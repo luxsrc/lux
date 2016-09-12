1194918958d3d95cfdf527e17b53f0f754f7b49e
Chi-kwan Chan <chanc@email.arizona.edu>
2016-09-12 02:36:10 -0700

    Add a test to check the planner

---
+++ tests/ck_planner.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016 Chi-kwan Chan
+ * Copyright (C) 2016 Steward Observatory
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <lux.h>
+#include <lux/planner.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+	lux_setup(&argc, &argv);
+
+	lux_print("1. Load solvers from \"planner_demo\" into planner...\n");
+
+	return 0;
+}
