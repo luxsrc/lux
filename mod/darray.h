ce0473a078d95cf5b03dab0a21bb92836140d43c
Chi-kwan Chan <chanc@email.arizona.edu>
2016-09-16 20:51:36 -0700

    Introduce <lux/darray.h> to separate the dope vector and the data
    It is unfortunate that the OpenCL standard does not specify cl_mem
    have to be a valid pointer.  Therefore, we cannot use the hacks we
    came up with for parray and are forced to keep track of the dope
    vector and data pointer separately.

---
+++ mod/darray.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2009 Chi-kwan Chan
+ * Copyright (C) 2009 Harvard-Smithsonian Center for Astrophysics
+ *
+ * This file is part of lux.
+ *
+ * lux is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lux is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lux.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _LUX_DARRAY_H_
+#define _LUX_DARRAY_H_
+
+#include <lux/dope.h>
+#include <stdlib.h> /* for malloc() and free() */
+
+struct darray {
+	struct dope *dope;
+	void        *data;
+};
+
+#define dalloc(T, a, ...) ({          \
+	size_t _n_[] = {__VA_ARGS__}; \
+	size_t _d_   = countof(_n_);  \
+	dallocdn(T, a, _d_, _n_);     \
+})
+
+#define dallocdn(T, a, D, Ns) ({                       \
+	struct darray darray;                          \
+	darray.dope = mkdopedn(sizeof(T), a, D, Ns);   \
+	darray.data = malloc(dope_getsz(darray.dope)); \
+	darray;                                        \
+})
+
+#define dfree(da) do {   \
+	rmdope(da.dope); \
+	free(da.data);   \
+} while(0)
+
+#define dgetd(da) pgetn(da.dope, 0)
+
+#define dgetn(da, j) dope_getn(da.dope+j)
+
+#endif /* _LUX_DARRAY_H_ */
